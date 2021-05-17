7ea4bf023d7103fe485be4b85d745a6399426eb4
Josh Vallon <joshua.vallon@gmail.com>
2021-05-17 14:32:48 -0700

    feat: install router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router';
 import store from './store'
 // import './services/webxpanel'
 
@@ -32,5 +33,6 @@ Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App),
 }).$mount('#app')
