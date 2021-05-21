836aafcdf410d142c7dfd3c235672536b6960943
Josh Vallon <joshua.vallon@gmail.com>
2021-05-20 20:31:19 -0700

    note: remove unused code

--- src/App.vue
+++ src/App.vue
@@ -14,21 +14,13 @@
       </div>
       <div class="footer"></div>
     </div>
-    <!-- <button @click="powerOffClick" :class="{ active: isOff }">Power Off</button>
-    <button @click="powerOnClick" :class="{ active: isOn }">Power On</button> -->
-    <!-- <display namespace="display1" name="d1" :config="this.disp1"></display>
-    <display namespace="displayTwo" name="display2" :config="this.disp2"></display> -->
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 
-import { mapState, mapActions } from 'vuex'
 import TheHeader from '@/components/Header/TheHeader';
 import NavList from '@/components/Navigation/NavList';
-import * as disp1 from '@/assets/data/tv.js';
-import * as disp2 from '@/assets/data/tv2.js';
 import navitems from '@/assets/data/navitems.json';
 
 export default {
@@ -39,31 +31,14 @@ export default {
   },
   data() {
     return {
-      navListItems: navitems,
-      disp1: disp1.default,
-      disp2: disp2.default
+      navListItems: navitems
     }
   },
-  methods: 
-      {
-      powerOffClick() {
-          // this.$store.dispatch('default/powerOff');
-      },
-      powerOnClick() {
-          // this.$store.dispatch('default/powerOn');
-      },
-      ...mapActions({
-      //   setName: 'default/setName'
-      }),
+  methods: {
   },
   computed: {
-    ...mapState([
-        // isOn: state => state.default.isOn,
-        // isOff: state => state.default.isOff
-    ]),
   },
   created() {
-    // this.$store.dispatch('default/subscribeAll');
   },
   mounted() {
   }
